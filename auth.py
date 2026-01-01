327b90c109aebda1e8ef71749501c82f687e3c4f
pamela <pamelavictory70@gmail.com>
2026-01-01 19:39:59 +0100

    second commit

--- auth.py
+++ auth.py
@@ -1,10 +1,22 @@
 from flask import Flask,jsonify,request
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_jwt_extended import (
+    JWTManager,
+    create_access_token,
+    jwt_required,
+    get_jwt_identity
+)
+
+
 
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///travel.db"
 db = SQLAlchemy(app)
+
+app.config["JWT_SECRET_KEY"] = "super-secret-key"  # change in real apps
+jwt = JWTManager(app)
 
 class User(db.Model):
     id= db.Column(db.Integer,primary_key=True)
@@ -25,7 +37,8 @@ def home():
 @app.route('/register',methods=["POST"])
 def register():
     user_data = request.get_json()
-    info = User(name=user_data['name'],email = user_data['email'],password= user_data['password'])
+    hashed_password = generate_password_hash(user_data["password"])
+    info = User(name=user_data['name'],email = user_data['email'],password= hashed_password)
     db.session.add(info)
     db.session.commit()
     return jsonify({"message":"registration successful"})
@@ -37,16 +50,19 @@ def login():
     email = user_data.get('email')
     password = user_data.get('password')
     user = User.query.filter_by(email=email).first()
-    if user:
-        return jsonify({"message": "login sucessful",
-                        "name":user_data["name"],
-                        "email":user_data["email"]
-        })
+    if  not user:
+        return jsonify({"error": "invalid email"}),400
+    if  check_password_hash(user.password,password):
+        access_token = create_access_token(identity=user.id)
+        return jsonify(access_token=access_token,)
     else:
-        return jsonify({"error":"this user does not exist"})
+        return jsonify({"error":"invalid login"}),400
+    
 
-
-
+@app.route("/content")
+@jwt_required()
+def content():
+    return jsonify({"message":"this returns the main content of the app"})
 
 
 
